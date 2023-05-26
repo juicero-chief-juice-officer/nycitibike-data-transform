635b8d690e1c27bc1cc233579e1bbaf79dd1a071
Stephen Hazeltine <stephenhazeltine@gmail.com>
2023-05-26 11:48:49 -0400

    go script update

--- dbt/nycitibike_transform/setup_script.sh
+++ dbt/nycitibike_transform/setup_script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#simple script to tell gcp what dbt commands should be run
+# simple script to tell gcp what dbt commands should be run
+# --profiles-dir is needed as dbt usually looks for profiles.yml in ~/.dbt/
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt debug --target dev --profiles-dir .
 dbt debug --target prod --profiles-dir .
