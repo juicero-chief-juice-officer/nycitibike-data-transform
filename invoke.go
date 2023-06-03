67c05c06f01dea6f48b4b7e0b4b6100531b7dde9
Stephen Hazeltine <stephenhazeltine@gmail.com>
2023-06-03 10:34:40 -0400

    update invoke.go

--- invoke.go
+++ invoke.go
@@ -1,35 +1,30 @@
-
-
-
-"""Cloud Run expects HTTP requests, but we run shell commands in dbt CLI. 
-		That’s why we need to set up a Go based HTTP server that calls a script.sh with our commands, when it receives a request. 
-		"""
-
 package main
+
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "os"
-        "os/exec"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
 )
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Request Received")
-	cmd := exec.Command("/bin/sh", "script.sh")
-			cmd.Stdout = os.Stdout
+	cmd := exec.Command("/bin/sh", "setup_script.sh")
+	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
-	log.Fatalf("cmd.Run() failed with %s\n", err)
+		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
 }
 func main() {
 	log.Print("Server starting")
 	http.HandleFunc("/", handler)
 	port := os.Getenv("PORT")
-			if port == "" {
-					port = "8080"
-			}
+	if port == "" {
+		port = "8080"
+	}
 	log.Printf("%s port listening", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
